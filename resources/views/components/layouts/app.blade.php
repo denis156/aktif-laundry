c40e68e9fdcd4aefc054f45a6a236b19f2182b2b
Denisa_13 <56766058+denis156@users.noreply.github.com>
2025-11-05 07:12:10 +0800

    style(ui): improve sidebar rounded corners for responsive design

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -34,7 +34,7 @@
     {{-- MAIN --}}
     <x-main full-width>
         {{-- SIDEBAR --}}
-        <x-slot:sidebar drawer="main-drawer" collapse-text="Tutup" right-mobile collapsible class="bg-base-100 lg:bg-base-100 rounded-l-xl md:rounded-r-2xl lg:rounded-r-4xl">
+        <x-slot:sidebar drawer="main-drawer" collapse-text="Tutup" right-mobile collapsible class="bg-base-100 lg:bg-base-100 rounded-l-xl rounded-r-none md:rounded-l-none md:rounded-r-2xl lg:rounded-l-none lg:rounded-r-4xl">
 
             {{-- BRAND --}}
             <x-app-brand class="px-5 pt-4" />
